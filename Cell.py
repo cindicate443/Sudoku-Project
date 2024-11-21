fad1b5ae8b97f2daf0e85746294b946ddf48bc52
peconnally <61524425+peconnally@users.noreply.github.com>
2024-11-21 15:52:56 -0500

    cell draft

--- Cell.py
+++ Cell.py
@@ -26,9 +26,16 @@ class Cell:
         if self.value == 0:
             return
         width = self.screen.get_width()
-        length = self.screen.get_length
+        length = self.screen.get_height()
         cell_w = width / 9
         cell_l = length / 9
-        top_left = (self.col*width, self.row*length)
+        top_left = (self.col*cell_w, self.row*cell_l)
         self.screen.blit(self.digits[self.value], self.digits[self.value].get_rect(topleft=top_left))
-
+        if self.selected:
+            color = "red"
+        else:
+            color = "black"
+        pygame.draw.line(self.screen, color=color, start_pos=top_left, end_pos=(top_left[0]+cell_w, top_left[1]))
+        pygame.draw.line(self.screen, color=color, start_pos=top_left, end_pos=(top_left[0], top_left[1]+cell_l))
+        pygame.draw.line(self.screen, color=color, start_pos=(top_left[0], top_left[1] + cell_l), end_pos=(top_left[0]+cell_w, top_left[1] + cell_l))
+        pygame.draw.line(self.screen, color=color, start_pos=(top_left[0]+cell_w,top_left[1]), end_pos=(top_left[0]+cell_w, top_left[1] + cell_l))
